9d19e2da97a67de9dc0d5fc511b17f0ba0ccdce5
randisihombing <randisihombing15@gmail.com>
2025-04-12 14:55:31 +0700

    fix port and db config

--- start-server.sh
+++ start-server.sh
@@ -1,10 +1,9 @@
+# start-server.sh
 #!/bin/bash
 set -e
 
-# Cache config dan jalankan migrasi
 php artisan config:clear
 php artisan cache:clear
-php artisan migrate --force
 
-# Gunakan PORT dari Railway atau default 8080
+# Gunakan port dari ENV atau default 8080
 php -S 0.0.0.0:${PORT:-8080} -t public
