bfb13eb8871190058732cfb3ac19673c6c397c18
randisihombing <randisihombing15@gmail.com>
2025-04-12 15:10:37 +0700

    add: start-server.sh for Railway deployment

--- start-server.sh
+++ start-server.sh
@@ -1,9 +1,8 @@
-# start-server.sh
 #!/bin/bash
 set -e
 
 php artisan config:clear
 php artisan cache:clear
 
-# Gunakan port dari ENV atau default 8080
-php -S 0.0.0.0:${PORT:-8080} -t public
+# Gunakan ini untuk production:
+php -S 0.0.0.0:8080 -t public public/index.php
